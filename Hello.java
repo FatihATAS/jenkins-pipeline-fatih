1c9b42ddaf50d462c0323d7169a70aacd3096df7
FatihATAS <fatihatas9324@gmail.com>
2023-01-27 21:19:06 +0300

    atas eklendi

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java fth");
+        System.out.println("Hello from Java fth atas");
     }
 }
